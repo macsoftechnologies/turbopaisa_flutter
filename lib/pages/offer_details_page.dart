36a18a5cdb218f66e00bbcf79c2a8adf5b17243d
Srinu <ramesh.kec.85@gmail.com>
2023-08-27 23:05:54 +0530

    offer_details_page added

---
+++ lib/pages/offer_details_page.dart
@@ -0,0 +1,190 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:offersapp/api/model/OffersData.dart';
+import 'package:offersapp/utils.dart';
+import 'package:offersapp/utils/app_colors.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class OfferDetailsPage extends StatefulWidget {
+  OffersData data;
+
+  OfferDetailsPage({Key? key, required this.data}) : super(key: key);
+
+  @override
+  State<OfferDetailsPage> createState() => _OfferDetailsPageState();
+}
+
+class _OfferDetailsPageState extends State<OfferDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.primaryColor,
+        title: Text("Diwali Offers"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  widget.data.images![0].image.toString(),
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                  height: 160,
+                ),
+              ),
+            ),
+            // SizedBox(
+            //   height: 20,
+            // ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Diwali Offers",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      Text("Register for diwali offer")
+                    ],
+                  ),
+                  Container(
+                    //width: 250,
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      color: AppColors.accentColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Text(
+                          "Get ₹ 50",
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Divider(
+              thickness: 2,
+              indent: 20,
+              endIndent: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "About application",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Icon(Icons.keyboard_arrow_down)
+                    ],
+                  ),
+                  Text(
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum doloreLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore",
+                      textAlign: TextAlign.justify),
+                ],
+              ),
+            ),
+            Divider(
+              thickness: 2,
+              indent: 20,
+              endIndent: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Task (0/1)",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Icon(Icons.keyboard_arrow_down)
+                    ],
+                  ),
+                  Text(
+                    "Install Now ",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text("Install the app in your smartphone"),
+                ],
+              ),
+            ),
+            Divider(),
+            SizedBox(
+              height: 10,
+            ),
+            //Spacer(),
+            RichText(
+              text: TextSpan(
+                text: '12000 ',
+                style: TextStyle(
+                  color: AppColors.accentColor,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                      text: 'User have already participated',
+                      style: TextStyle(color: Colors.black)),
+                ],
+              ),
+            ),
+
+            InkWell(
+              onTap: () {
+                _launchUrl(widget.data.url ?? "");
+              },
+              child: Container(
+                width: 250,
+                padding: EdgeInsets.all(8),
+                decoration: BoxDecoration(
+                  color: AppColors.accentColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Text(
+                      "Register Now",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+
+  HtmlEscape htmlEscape = HtmlEscape();
+
+  Future<void> _launchUrl(String _url) async {
+    if (!await launchUrl(Uri.parse(_url))) {
+      // throw Exception('Could not launch $_url');
+      showSnackBar(context, 'Could not launch $_url');
+    }
+  }
+}
