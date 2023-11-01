117747f3c0802af0980f13da9471b96bba92d863
Ramesh Akula <ramesh.akula@otis.com>
2023-11-01 22:34:03 +0530

    Release first version.

--- lib/utils/ad_helper.dart
+++ lib/utils/ad_helper.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      // return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-6960940623702317/4699448903';
     } else if (Platform.isIOS) {
       return 'ca-app-pub-3940256099942544/2934735716';
     } else {
